5dc3d84bd65b7f7a7fe3c2284d35f4ade2720507
opasnatikva@gmail.com <T0mbRaider>
2020-11-10 00:07:03 +0200

    First iteration of the game, working random population of numbers and game loop

--- Puzzle15.py
+++ Puzzle15.py
@@ -2,6 +2,7 @@ import random
 
 BOARD_SIZE = 4
 BLANK_STATE = '  '
+solved = False
 
 
 def generate_board(size=BOARD_SIZE):
@@ -15,96 +16,137 @@ def generate_board(size=BOARD_SIZE):
 
 
 def print_board(board):
-    for index in range(len(board)):
-        print(board[index])
+    for row_index in range(len(board)):
+        row = []
+        for col_index in range(len(board)):
+            row.append(board[col_index][row_index])
+        print(row)
 
 
 def number_sequence(size=BOARD_SIZE):
     """Creates a serial list of numbers with length equal to the number of fields on the board.
     This list will be used to populate the board, 0 is replaced with two blank spaces to retain formatting."""
-    list_of_numbers = [BLANK_STATE]
+    list_of_numbers = []
     for index in range(1, (size * size)):
         list_of_numbers.append(format(index, '02d'))
+    list_of_numbers.append(BLANK_STATE)
     return list_of_numbers
 
 
-def generate_new_random_number(numbers):
-    """Chooses a number from the list at random and deletes it from the list"""
-    number = random.choice(numbers)
-    numbers.remove(number)
-    return number
+def puzzle_state(board, size=BOARD_SIZE):
+    """Takes the nested lists and arranges all values in a single list in order to then compare them to the solution"""
+    order = []
+    for row_index in range(size):
+        order.append([])
+        for col_index in range(size):
+            order.append(board[row_index][col_index])
+    return order
+
+
+def compare_to_solution(puzzle_state, number_sequence):
+    if puzzle_state == number_sequence:
+        return True
+
+
+# def generate_new_random_number(numbers):
+#     """Chooses a number from the list at random and deletes it from the list"""
+#     number = random.choice(numbers)
+#     numbers.remove(number)
+#     return number
 
 
 def populate_board(board):
     """Initiate the number sequence, then cycle through the cells of the board, and populate them with it"""
     numbers = number_sequence()
-    for row_index in range(len(board)):
-        for col_index in range(len(board)):
-            board[row_index][col_index] = generate_new_random_number(numbers)
+    for col_index in range(len(board)):
+        for row_index in range(len(board)):
+            board[row_index][col_index] = numbers[0]
+            numbers.remove(numbers[0])
 
 
 def find_empty_square(board):
-    for col_index in range(len(board)):
-        for row_index in range(len(board)):
-            if board[col_index][row_index] == BLANK_STATE:
+    for row_index in range(len(board)):
+        for col_index in range(len(board)):
+            if board[row_index][col_index] == BLANK_STATE:
                 return row_index, col_index
 
 
-def user_input():
+def shuffle_input():
+    random_moves = input("insert as many WASD letters as you want in order to shuffle the board")
+    return random_moves.lower()
+
+
+# def randomize(board, sequence, empty_square_x, empty_square_y):
+#     for index in sequence:
+#
+
+
+def input_and_border_check(empty_square_x, empty_square_y):
+    """Take input from user, checks if what is provided is a single directional letter,
+     and checks whether the move would be appropriate for the current blank field"""
     correct_input = False
     while not correct_input:
         direction = input("Please select which direction your desired would have to move"
                           " in order to fill the empty square! W for up, S for down, A for left, and D for right.")
         direction = direction.lower()
-        if direction == "w" or "a" or "s" or "d":
-            correct_input = True
+        if direction == 's':
+            if empty_square_y > 0:
+                correct_input = True
+        elif direction == 'w':
+            if empty_square_y < (BOARD_SIZE - 1):
+                correct_input = True
+        elif direction == 'd':
+            if empty_square_x > 0:
+                correct_input = True
+        elif direction == 'a':
+            if empty_square_x < (BOARD_SIZE - 1):
+                correct_input = True
+        else:
+            direction = 'exception'
     return direction
 
 
-def input_and_border_check(empty_square_x, empty_square_y):
-    correct_input = False
-    while not correct_input:
-        direction = user_input()
-        if direction == 'w':
-            if empty_square_y < 1:
-                break
-        elif direction == "s":
-            if empty_square_y > (BOARD_SIZE - 1):
-                break
-        elif direction == "a":
-            if empty_square_x < 1:
-                break
-        elif direction == "d":
-            if empty_square_x > (BOARD_SIZE - 1):
-                break
-    return direction
-
-
-def move(board, empty_square_x, empty_square_y,):
+def move(board, empty_square_x, empty_square_y):
+    """Takes the board, and the empty square, and based on the input overwrites the value of the empty square,
+     and makes the source field the new empty field"""
     direction = input_and_border_check(empty_square_x, empty_square_y)
+    empty_x = empty_square_x
+    empty_y = empty_square_y
     if direction == 'w':
         board[empty_square_x][empty_square_y] = board[empty_square_x][empty_square_y + 1]
         board[empty_square_x][empty_square_y + 1] = BLANK_STATE
+        empty_y = empty_square_y + 1
     elif direction == "s":
         board[empty_square_x][empty_square_y] = board[empty_square_x][empty_square_y - 1]
         board[empty_square_x][empty_square_y - 1] = BLANK_STATE
+        empty_y = empty_square_y - 1
     elif direction == "a":
-        board[empty_square_x][empty_square_y] = board[empty_square_x - 1][empty_square_y]
-        board[empty_square_x - 1][empty_square_y] = BLANK_STATE
-    elif direction == "d":
         board[empty_square_x][empty_square_y] = board[empty_square_x + 1][empty_square_y]
         board[empty_square_x + 1][empty_square_y] = BLANK_STATE
+        empty_x = empty_square_x + 1
+    elif direction == "d":
+        board[empty_square_x][empty_square_y] = board[empty_square_x - 1][empty_square_y]
+        board[empty_square_x - 1][empty_square_y] = BLANK_STATE
+        empty_x = empty_square_x - 1
+    else:
+        return empty_x, empty_y
+    return empty_x, empty_y
 
 
 def main():
     board = generate_board()
     populate_board(board)
     print_board(board)
-    empty_square_x = find_empty_square(board)[0]
-    empty_square_y = find_empty_square(board)[1]
-    print(find_empty_square(board))
-    move(board, empty_square_x, empty_square_y)
-    print_board(board)
+    empty_square_x, empty_square_y = find_empty_square(board)
+
+    while True:
+        empty_square_x, empty_square_y = move(board, empty_square_x, empty_square_y)
+        print_board(board)
+
 
 if __name__ == '__main__':
     main()
+
+
+"""Move да приема само direction string и ако е грешен, да не го изпълнява, 
+така когато randomize-ваме няма да имаме проблеми с boundary когато вкараме random стринг от asdwasdwasdwad..."""
